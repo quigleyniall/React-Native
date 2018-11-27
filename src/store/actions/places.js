17dd1217568d4139b3ee5ee384bab969a66961c5
Niall Quigley <niallquigley92@gmail.com>
2018-11-27 21:15:04 +0000

    Add redux thunk

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,10 +1,20 @@
 import { ADD_PLACE, DELETE_PLACE } from "./actionTypes";
 
 export const addPlace = (placeName, location) => {
-  return {
-    type: ADD_PLACE,
-    placeName: placeName,
-    location: location
+  return dispatch => {
+    const placeData = {
+      name: placeName,
+      location: location
+    };
+    fetch("https://react-native-pro-1543318711113.firebaseio.com/places.json", {
+      method: "POST",
+      body: JSON.stringify(placeData)
+    })
+      .catch(err => console.error(err))
+      .then(res => res.json())
+      .then(parsedResp => {
+        console.log(parsedResp);
+      });
   };
 };
 
